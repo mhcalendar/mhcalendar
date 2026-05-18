2ea7ccab27916a920e29742dc17954c316f6c5b6
mateusz <mateusz@mat.pl>
2026-05-18 14:30:08 +0200

    update examples

---
+++ examples/react-19.2-vite/src/App.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { MhCalendar, type IMHCalendarEvent, type IMHCalendarViewType } from '@mhcalendar/react';
+import './App.css';
+
+const CONFIG = {
+  viewType: 'MONTH' as IMHCalendarViewType,
+  showTimeFrom: 8,
+  showTimeTo: 18,
+  showAllDayTasks: true,
+  allowEventDragging: true,
+  allowEventResize: true,
+  createEventOnClick: false,
+};
+
+const TEST_EVENTS = [
+  {
+    id: 'rand-id-1',
+    startDate: new Date(new Date().setHours(12)),
+    endDate: new Date(new Date().setHours(15)),
+    title: 'rand-id-1',
+  },
+  {
+    id: 'rand-id-2',
+    startDate: new Date(new Date().setHours(9)),
+    endDate: new Date(new Date().setHours(11)),
+    title: 'rand-id-1',
+  },
+  {
+    id: 'rand-id-3',
+    startDate: new Date(new Date().setHours(17)),
+    endDate: new Date(new Date().setHours(20)),
+    title: 'rand-id-1',
+  },
+] as IMHCalendarEvent[];
+
+function App() {
+  const [events, setEvents] = useState(TEST_EVENTS);
+
+  return (
+    <main className="app">
+      <MhCalendar config={{ ...CONFIG }} events={events} />
+    </main>
+  );
+}
+
+export default App;

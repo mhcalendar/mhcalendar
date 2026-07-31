a87dbcaafa1941e68b5b3a1e9224e90b9e9c3659
mateusz <mateusz@mat.pl>
2026-07-31 12:57:26 +0200

    fix error messages

--- packages/calendar/src/utils/ConfigValidator/error-messages.ts
+++ packages/calendar/src/utils/ConfigValidator/error-messages.ts
@@ -5,7 +5,8 @@ export const ERROR_MESSAGES = {
     'Both fixedHeight and virtualScrollHeight must be set for virtual scrolling to work.',
   [ConfigErrorCodes.SHOW_TIME_HOURS]:
     'showTimeFrom and showTimeTo must be numbers between 0 and 24, with showTimeTo greater than showTimeFrom',
-  [ConfigErrorCodes.SLOT_TIME_HOURS]: 'Need to have correct shape',
+  [ConfigErrorCodes.SLOT_TIME_HOURS]:
+    'slotInterval and hoursSlotInterval must be objects with both "hours" and "minutes" properties.',
   [ConfigErrorCodes.SLOT_MINUTES_DIVISIBLE]:
     'slotInterval.minutes and hoursSlotInterval.minutes must be divisible by 5 (e.g. 0, 5, 10, 15, 30)',
   [ConfigErrorCodes.TIMEZONES_1]: 'Timezones were given with wrong format. Use e.g "Europe/Warsaw"',
--- packages/calendar/src/utils/ConfigValidator/warning-messages.ts
+++ packages/calendar/src/utils/ConfigValidator/warning-messages.ts
@@ -2,5 +2,5 @@ import { WarningCodes } from './warning-codes';
 
 export const WARNING_MESSAGES = {
   [WarningCodes.TIMEZONE_LIMIT_EXCEEDED]:
-    'Timezones are limited to a three at most. Every timezone above that limit will not be used.',
+    'Timezones are limited to three at most. Every timezone above that limit will not be used.',
 };

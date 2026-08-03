13e326036a4e8f9c9fc095490feb59ffdb543fdc
mateusz <mateusz@mat.pl>
2026-08-03 12:31:16 +0200

    Update section about props

--- packages/calendar/src/const/default-config.ts
+++ packages/calendar/src/const/default-config.ts
@@ -47,8 +47,6 @@ export const DEFAULT_WEEK_VIEW_CONFIG: IMHCalendarWeekConfig = {
   timezoneLabel: undefined,
   eventDisplayMode: EventDisplayMode.SideBySide,
   showTimeIndicator: true,
-  customWeekView: false,
-  showWeekends: true,
   resources: [],
   shiftplanDays: 7,
 };
